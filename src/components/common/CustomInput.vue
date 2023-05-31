3f66915d0cf9343175ba8753463d1486d513980a
이현규 <aiden@guarantable.com>
2023-05-31 23:26:25 +0900

    [UPDATE] CustomInput

---
+++ src/components/common/CustomInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <input type="text" />
+</template>
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "LoadingButton",
+  props: {},
+  components: {},
+  setup() {
+    const loading = ref(false);
+    return {
+      loading,
+    };
+  },
+});
+</script>
+<style lang="scss"></style>
